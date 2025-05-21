714c52e9c4586532c2832ba9f759d25fe140e2bd
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-21 11:22:17 +0600

    Update 5LoginWithNewCred.Spec.js
    added negetive test case

--- tests/5LoginWithNewCred.Spec.js
+++ tests/5LoginWithNewCred.Spec.js
@@ -2,14 +2,25 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import jsonData from "../resources/Updatedcredentials.json";
-const latestUserData = jsonData;
+import jsonData2 from "../resources/userData.json";
 
 test("Login with New Credentials", async ({ page }) => {
   await page.goto("/");
-
+  const latestUserData = jsonData;
   const login = new LoginPage(page);
 
   await login.doLogin(latestUserData.email, latestUserData.password);
 
-  await expect(page.getByText("Dashboard")).toBeVisible({ timeout: 20000 });
+  await expect(page.getByText("Dashboard")).toBeVisible({ timeout: 30000 });
+});
+
+test("Try to Login with Old Credentials", async ({ page }) => {
+  await page.goto("/");
+
+  const PreviousUserCred = jsonData2[jsonData2.length - 1];
+  const login = new LoginPage(page);
+  await login.doLogin(PreviousUserCred.email, PreviousUserCred.password);
+  await expect(page.getByRole("paragraph")).toContainText(
+    "Invalid email or password"
+  );
 });
